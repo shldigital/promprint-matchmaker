17c258f8da5c4292d3c69eec735835923e1ee559
Nic Seymour-Smith <nrs23@sussex.ac.uk>
2025-12-08 14:36:30 +0000

    [tests] fix main tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,12 +6,12 @@ register_file = "./tests/test_files/test_register_export.csv"
 bad_register_file = "./tests/test_files/bad_file.txt"
 
 
-def test_loads_register():
-    test_args = [register_file]
+def test_loads_register(tmp_path):
+    test_args = [register_file, str(tmp_path)]
     main(test_args)
 
 
-def test_bad_file_raises():
-    test_args = [bad_register_file]
+def test_bad_file_raises(tmp_path):
+    test_args = [bad_register_file, str(tmp_path)]
     with pytest.raises(KeyError):
         main(test_args)
