678f87cdc2422f3a34d5e10639baddd6b01e34dd
Nic Seymour-Smith <nrs23@sussex.ac.uk>
2025-12-08 16:32:06 +0000

    [feat] Match short titles only against start of other title

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,5 +1,24 @@
+"""Helper functions for text matching."""
 from thefuzz import fuzz
 
 
 def match_score(text_1: str, text_2: str) -> int:
+    """
+    Return the similary score of two input texts.
+
+    Expects cleaned, space separated tokens for each text input
+    Shorter texts are only matched at the beginning of longer texts.
+
+    :param text_1: First piece of text to match
+    :type text_1: str
+    :param text_2: Second piece of text to match
+    :type text_2: str
+    :return: Match score indicating how similar the texts are
+    :rtype: int
+    """
+    toks = [text_1.split(" "), text_2.split(" ")]
+    toks.sort(key=len)
+    if len(toks[0]) < 4:
+        text_1 = " ".join(toks[0])
+        text_2 = " ".join(toks[1][:4])
     return fuzz.partial_ratio(text_1, text_2)
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -9,6 +9,23 @@ four_token_partial_match_data = [
     ("quick brown fox jumps", "brown fox jumps over", 86),
 ]
 
+# Note that if the shorter string is less than 3 tokens long then we're only
+# interested if it matches at the *start* of the other string
+# This assumes "title:subtitle" structure (which may not always be the case)
+# and thereby filters out junk matches of very short texts in very long texts
+three_token_partial_match_data = [
+    ("quick brown fox", "the quick brown fox jumps over the lazy dog", 100),
+    ("the lazy dog", "the quick brown fox jumps over the lazy dog", 50),
+    ("novellos collections of the favourite songs andc", "songs", 60),
+    ("songs", "novellos collections of the favourite songs andc", 60),
+    ("dorset", "worse than death etc", 83)]
+
+
 @pytest.mark.parametrize("text_1, text_2, score", four_token_partial_match_data)
 def test_four_token_partial_match_score(text_1, text_2, score):
+    assert match_score(text_1, text_2) == score
+
+
+@pytest.mark.parametrize("text_1, text_2, score", three_token_partial_match_data)
+def test_three_token_partial_match_score(text_1, text_2, score):
     assert match_score(text_1, text_2) == score
