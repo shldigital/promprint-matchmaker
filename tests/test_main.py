8d5ab0cae9c4b03e97dc589597911da58075a9a5
Nic Seymour-Smith <nrs23@sussex.ac.uk>
2026-01-09 11:40:54 +0000

    [tests] Fix test fail since reordering of args

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,11 +8,11 @@ bad_register_file = "./tests/test_files/bad_file.txt"
 
 
 def test_good_register(tmp_path):
-    test_args = [register_file, str(tmp_path), collection_file]
+    test_args = [register_file, collection_file, str(tmp_path)]
     main(test_args)
 
 
 def test_bad_file_raises(tmp_path):
-    test_args = [bad_register_file, str(tmp_path), collection_file]
+    test_args = [bad_register_file, collection_file, str(tmp_path)]
     with pytest.raises(KeyError):
         main(test_args)
