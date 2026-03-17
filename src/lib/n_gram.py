31e75227900352d6579c8a080447c989a3345237
Nic Seymour-Smith <nrs23@sussex.ac.uk>
2026-03-17 16:26:22 +0000

    [WIP] function for n grams over multiple degrees

--- src/lib/n_gram.py
+++ src/lib/n_gram.py
@@ -2,7 +2,7 @@ import pandas as pd
 from nltk import ngrams
 
 
-def n_gram_frequency(text_series: pd.Series, degree: int = 1) -> pd.DataFrame:
+def n_gram_frequency(text_series: pd.Series, degree: int = 1) -> pd.Series:
     n_gram_series = pd.Series(name="n_gram")
 
     for row in text_series:
@@ -15,3 +15,18 @@ def n_gram_frequency(text_series: pd.Series, degree: int = 1) -> pd.DataFrame:
             )
 
     return n_gram_series.value_counts()
+
+
+def multi_n_gram_frequency(
+    text_series: pd.Series, min_degree: int = 1, max_degree: int = None
+) -> pd.Series:
+    n_gram_frequencies = pd.Series()
+    if max_degree is None:
+        max_degree = text_series.map(lambda t: len(t.split())).max()
+
+    # TODO: tokenize once, pass tokenized lists to the n_gram_frequency function
+    for degree in range(min_degree, max_degree+1):
+        new_n_gram_frequencies = n_gram_frequency(text_series, degree)
+        n_gram_frequencies = pd.concat([n_gram_frequencies, new_n_gram_frequencies])
+
+    return n_gram_frequencies.sort_values(ascending=False)
--- tests/test_n_gram_list.py
+++ tests/test_n_gram_list.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pytest
 
 from src.cli.n_gram_list import main
-from lib.n_gram import n_gram_frequency
+from lib.n_gram import n_gram_frequency, multi_n_gram_frequency
 from pandas.testing import assert_series_equal
 from pathlib import Path
 
@@ -29,7 +29,7 @@ def test_missing_column_throws(tmp_path):
         main(tmp_path, test_collections, ["spurious_column"], 20, 100)
 
 
-test_column = [
+test_data = [
     "the",
     "the quick",
     "the quick brown",
@@ -73,9 +73,65 @@ expected_frequencies = [
 
 @pytest.mark.parametrize("degree, expected_data", expected_frequencies)
 def test_n_gram_frequency(degree, expected_data):
-    test_series = pd.Series(test_column)
+    test_series = pd.Series(test_data)
     n_gram_df = n_gram_frequency(test_series, degree)
     n_gram_df.to_csv(temp_output / "n_gram_count.csv")
     expected_data_series = pd.Series(data=expected_data)
     expected_data_series.name = "count"
+    assert_series_equal(n_gram_df, expected_data_series)
+
+
+expected_data = {
+    "the": 12,
+    "quick": 8,
+    "the quick": 8,
+    "brown": 7,
+    "quick brown": 7,
+    "the quick brown": 7,
+    "brown fox": 6,
+    "fox": 6,
+    "quick brown fox": 6,
+    "the quick brown fox": 6,
+    "fox jumps": 5,
+    "jumps": 5,
+    "quick brown fox jumps": 5,
+    "the quick brown fox jumps": 5,
+    "brown fox jumps": 5,
+    "over": 4,
+    "quick brown fox jumps over": 4,
+    "the quick brown fox jumps over": 4,
+    "fox jumps over": 4,
+    "brown fox jumps over": 4,
+    "jumps over": 4,
+    "jumps over the": 3,
+    "the quick brown fox jumps over the": 3,
+    "fox jumps over the": 3,
+    "brown fox jumps over the": 3,
+    "quick brown fox jumps over the": 3,
+    "over the": 3,
+    "lazy": 2,
+    "the lazy": 2,
+    "quick brown fox jumps over the lazy": 2,
+    "over the lazy": 2,
+    "jumps over the lazy": 2,
+    "brown fox jumps over the lazy": 2,
+    "the quick brown fox jumps over the lazy": 2,
+    "fox jumps over the lazy": 2,
+    "dog": 1,
+    "the lazy dog": 1,
+    "lazy dog": 1,
+    "jumps over the lazy dog": 1,
+    "over the lazy dog": 1,
+    "fox jumps over the lazy dog": 1,
+    "brown fox jumps over the lazy dog": 1,
+    "quick brown fox jumps over the lazy dog": 1,
+    "the quick brown fox jumps over the lazy dog": 1,
+}
+
+
+def test_multi_n_gram_frequency():
+    test_series = pd.Series(test_data)
+    n_gram_df = multi_n_gram_frequency(test_series)
+    n_gram_df.to_csv(temp_output / "n_gram_count.csv")
+    expected_data_series = pd.Series(data=expected_data)
     assert_series_equal(n_gram_df, expected_data_series)
